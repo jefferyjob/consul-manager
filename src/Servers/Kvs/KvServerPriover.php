e8a167259f52218b363495f6dcda38d4f980f8d2
libin <bin.li@gaea.com>
2021-08-12 13:52:07 +0800

    init

---
+++ src/Servers/Kvs/KvServerPriover.php
@@ -0,0 +1,17 @@
+<?php
+namespace Jefferyjob\ConsulManager\Servers\Kvs;
+
+use Jefferyjob\ConsulManager\App\Kernel\ServerPriovderInterface;
+
+class KvServerPriover extends ServerPriovderInterface
+{
+    public function boot()
+    {
+        // TODO: Implement boot() method.
+    }
+
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+}
---
+++ src/Servers/Nodes/NodeServerPriover.php
@@ -0,0 +1,17 @@
+<?php
+namespace Jefferyjob\ConsulManager\Servers\Nodes;
+
+use Jefferyjob\ConsulManager\App\Kernel\ServerPriovderInterface;
+
+class NodeServerPriover extends ServerPriovderInterface
+{
+    public function boot()
+    {
+        // TODO: Implement boot() method.
+    }
+
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+}
