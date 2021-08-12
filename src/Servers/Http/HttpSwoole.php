e5b0485f3e2f18b7d6406f47da18d044a6cdfaaa
libin <bin.li@gaea.com>
2021-08-12 16:14:32 +0800

    swoole-http

--- src/Servers/Http/HttpSwoole.php
+++ src/Servers/Http/HttpSwoole.php
@@ -2,7 +2,14 @@
 namespace Jefferyjob\ConsulManager\Servers\Http;
 
 use Jefferyjob\ConsulManager\App\Application;
-
+use Swoole\Coroutine\Http\Client;
+/**
+ * --------------------------------------------------------------------------
+ * swoole实现http服务
+ * --------------------------------------------------------------------------
+ *
+ * wiki：https://wiki.swoole.com/#/coroutine_client/http_client
+ */
 class HttpSwoole implements HttpInterface
 {
     /**
@@ -10,8 +17,25 @@ class HttpSwoole implements HttpInterface
      */
     public $token;
 
+    /**
+     * @var object swoole-http客户端
+     */
+    private $client;
+
     public function __construct(Application $application) {
         $this->token = $application->token;
+        $this->init($application->host, $application->port);
+    }
+
+    public function init($host, $port)
+    {
+        $client = new Client($host, $port);
+        if($this->token) {
+            $client->setHeaders([
+                'X-Consul-Token' => $this->token
+            ]);
+        }
+        $this->client = $client;
     }
 
     public function get() {
